f890bcbb043cc6524b57e126b46baae880b1c9d0
bigbigrock <1562028916@qq.com>
2020-11-13 18:25:27 +0800

    结果不正确

--- test_cases/test_eth_sign.py
+++ test_cases/test_eth_sign.py
@@ -34,17 +34,6 @@ cases,list_params = get_test_data(date_path + 'test_eth_sign')
 #         assert response['id'] == expected['id'],"响应结果为：{}".format(response['result'])
 
 
-def test_hello():
-    r=requests.request('post',"http://123.56.170.101:8545",
-                       headers ={"Content-Type": "application/json" },
-                       data = { "json": 2,"method": "eth_sign","params": ["0xe2043a5B808bf2215D94e0934182E224a38A47D7","0xdeadbeaf"]
-        } )
-    response = r.json()  # 返回字典格式
-    return response
-    # assert response['id'] == 1, "响应结果为：{}".format(response['result'])
-
-a = test_hello()
-print(a)
 
 
 
